b7a2c647ccf900f8f941762c997f69a6bf1917bd
proin <proin@proinlab.com>
2021-10-13 13:48:37 +0900

    debug log socket

--- socket/index.py
+++ socket/index.py
@@ -1,9 +1,23 @@
-import season
+import sys
 
 class Controller:
     def __init__(self, framework):
-        self.framework = framework
-        self.register = ["response"]
+        _stdout = sys.stdout
+        class stdout():
+            def __init__(self, socketio):
+                self.socketio = socketio
+                
+            def write(self, string):
+                self.socketio.emit("log", string, namespace="/wiz", broadcast=True)
+                _stdout.write(string)
+
+            def flush(self):
+                _stdout.flush()
+
+        sys.stdout = stdout(framework.socketio)
+
+    def __startup__(self, framework):
+        framework.socket.emit("startup /wiz socket")
 
     def response(self, framework, data):
-        framework.socket.emit("hello")
+        framework.socket.emit("hello world")
