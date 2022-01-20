bc7f9df99fb08b11dadcf191a2359e9f40237120
KwonTaewook <kwon3286@season.co.kr>
2022-01-20 15:46:04 +0900

    주석 삭제

--- src/season/websrc/modules/wiz/model/plugin.py
+++ src/season/websrc/modules/wiz/model/plugin.py
@@ -548,20 +548,6 @@ class Model:
 
         fs.write_json(info_file, info)
 
-    # def zip(self, plugin_id, plugin_name):
-    #     fs = self.fs
-    #     path = fs.abspath(plugin_id)
-    #     zippath = os.path.join(path, f"{plugin_name}.zip")
-    #     os.system(f"zip -j -r '{zippath}' {path}")
-    #     return zippath
-
-    # def del_zip(self, plugin_id, plugin_name):
-    #     fs = self.fs
-    #     path = fs.abspath(plugin_id)
-    #     zippath = os.path.join(path, f"{plugin_name}.zip")
-    #     fs.delete(zippath)
-    #     return True
-
     def install(self, plugin_id, **data):
         fs = self.fs
         info_file = os.path.join(plugin_id, "plugin.wiz")
