92eea05b607ca048f6926dfb14eeccb604f4313d
proin <proin@season.co.kr>
2022-06-11 17:46:26 +0900

    1.0.3

--- README.md
+++ README.md
@@ -134,6 +134,10 @@ path = "/var/www/wiki/data"
 
 ## Release Note
 
+### 1.0.3
+
+- WSGI Bug Fixed
+
 ### 1.0.2
 
 - remove dukpy (windows install bug)
--- src/season/data/public/app.py
+++ src/season/data/public/app.py
@@ -1,7 +1,11 @@
+import os
 import sys
+PATH_CWD = os.path.dirname(os.path.dirname(__file__))
+os.chdir(PATH_CWD)
 import season
 
+server = season.Server()
 if __name__ == '__main__':
-    server = season.Server()
     server.run()
-    
+
+app = server.wsgi.flask
