7314be3c684184856c8ab6b3d6022d3fe18c3717
KwonTaewook <nanoha@kakao.com>
2024-05-02 15:53:11 +0900

    Fix: bug fixed controller.py

--- src/season/data/sample/src/portal/season/route/pwa.swjs/controller.py
+++ src/season/data/sample/src/portal/season/route/pwa.swjs/controller.py
@@ -1,4 +1,4 @@
 import os
-fs = wiz.workspace("service").fs(os.path.join("config", "pwa"))
+fs = wiz.project.fs(os.path.join("config", "pwa"))
 swjs = fs.read("sw.js", "")
 wiz.response.send(swjs, content_type="text/javascript; charset=utf-8")
